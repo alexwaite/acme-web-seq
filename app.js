ebd7770fcfe2fd402468000b89a9889d2f6ef620
Alex Waite <alexcwaite@gmail.com>
2019-02-16 17:10:23 -0500

    added forms to add and delete content

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const ejs = require('ejs');
 
+const methodOverride = require('method-override');
+
 const db = require('./db');
 const { Page, Content } = require('./db');
 
@@ -9,6 +11,8 @@ module.exports = app;
 
 app.set('view engine', 'html');
 app.engine('html', ejs.renderFile);
+
+app.use(methodOverride('_method'));
 
 app.get('/', (req, res, next) => {
   Page.findOne({
@@ -28,5 +32,27 @@ app.get('/pages/:id', (req, res, next) => {
     Page.findAll(),
   ])
     .then(([page, pages]) => res.render('index', { page, pages }))
+    .catch(next);
+});
+
+app.post('/pages/:id/contents', (req, res, next) => {
+  Content.create({
+    pageId: req.params.id,
+    name: 'A num',
+    body: `Math.random()`,
+  })
+    .then(content => res.redirect(`/pages/${req.params.id}`))
+    .catch(next);
+});
+
+app.delete('/pages/:pageId/contents/:id', (req, res, next) => {
+  Content.destroy({
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then(() => {
+      res.redirect(`/pages/${req.params.pageId}`);
+    })
     .catch(next);
 });
